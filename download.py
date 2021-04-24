fa99c5a10e7033cc13fc6939dfab489c3dedfdcd
Krishna Chaitanya <capriciousduck@gmail.com>
2021-04-24 12:39:51 +0530

    Update download.py

--- download.py
+++ download.py
@@ -1,7 +1,9 @@
 # Downloads model(s) during docker build
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-model_name = 'distilgpt2'
+#model_name = 'distilgpt2'
+
+model_name = '1558M'
 
 GPT2LMHeadModel.from_pretrained(model_name).save_pretrained('./gpt2')
 GPT2Tokenizer.from_pretrained(model_name).save_pretrained('./gpt2')
